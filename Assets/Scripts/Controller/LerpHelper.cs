95e67308a09f78c327dbf34e9f31f82b77fbd5ab
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-02-18 22:09:39 -0300

    Adding obstacles

--- Assets/Scripts/Controller/LerpHelper.cs
+++ Assets/Scripts/Controller/LerpHelper.cs
@@ -1,15 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class LerpHelper : MonoBehaviour
 {
     public Transform target;
-
     public float lerpSpeed = 1f;
+
+    [Header("FollowPlayer")]
+    public float speed_p = 1f;
+
+    private Vector3 _pos;
+
+    //public bool _canRunLerp;
+
+    //public string EnemyTag = "Enemy";
+
+
+    private void Start()
+    {
+        //_canRunLerp = true;
+    }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed * Time.deltaTime);
+        //if (!_canRunLerp) return;
+        PositionPlayer();
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        transform.Translate(transform.forward * speed_p * Time.deltaTime);
     }
+
+    void PositionPlayer()
+    {
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+    }
+
+  /*  public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == EnemyTag)
+        {
+            _canRunLerp = false;
+        }
+    }*/
 }
+
+// sem um rigdbody para parar 
+// como posso fazer ele seguir o player e parar quando ele parar ?
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public
+    public float speed = 1f;
+    [Header("Tag")]
+    public string EnemyTag = "Enemy";
+
+    //private
+    [Header("EndGame")]
+    public bool _canRun;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canRun = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_canRun) return;
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == EnemyTag) 
+        {
+            _canRun = false;
+        }
+    }
+}
