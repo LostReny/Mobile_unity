8b5b747e07814cd5dc3a09753b08be7ab73f65ff
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-02-27 23:48:29 -0300

    Coin Power Up

--- Assets/Scripts/Itens/Collectibles/ItemCollectCoin.cs
+++ Assets/Scripts/Itens/Collectibles/ItemCollectCoin.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemCollectCoin : ItemCollectBase
 {
@@ -10,12 +11,44 @@ public class ItemCollectCoin : ItemCollectBase
     public float minDistance = 1f;
 
 
+    private void Start()
+    {
+        //CoinsAnimationManager.Instance.RegisterCoin(this);
+    }
 
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemCollectManager.Instance.AddCoins();
         _collider.enabled = false;
+        collect = true;
+        //PlayerController.Instance.Bounce();
     }
-    
-}
+
+    protected override void Collect()
+    {
+        OnCollect();
+    }
+    private void Update()
+    {
+        if (collect)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+           PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+            if (Vector3.Distance(transform.position, PlayerController.Instance.transform.position) <
+           minDistance)
+            {
+                Invoke("HideObject", timeToHide);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+        /*protected override void OnCollect()
+        {
+            base.OnCollect();
+            ItemCollectManager.Instance.AddCoins();
+            _collider.enabled = false;
+        }*/
+
+    }
--- Assets/Scripts/Itens/PowerUps/PowerUpCoins.cs
+++ Assets/Scripts/Itens/PowerUps/PowerUpCoins.cs
@@ -11,10 +11,12 @@ public class PowerUpCoins : PowerUpBase
     {
         base.StartPowerUp();
         PlayerController.Instance.ChangeCoinCollectorSize(sizeAmount);
+        PlayerController.Instance.SetPowerUpText("Coins Buster");
     }
     protected override void EndPowerUp()
     {
         base.EndPowerUp();
         PlayerController.Instance.ChangeCoinCollectorSize(1);
+        PlayerController.Instance.SetPowerUpText("");
     }
 }
