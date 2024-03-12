97cf9fd28c19b0df0b6a203e43b5bcb106d6e8cf
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-03-12 11:03:40 -0300

    Art Type Change Done

--- Assets/Scripts/ArtPiece/ArtPiece.cs
+++ Assets/Scripts/ArtPiece/ArtPiece.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ArtPiece : MonoBehaviour
 {
     public GameObject currentArt;
 
+    public Transform artContainer;
 
-    public void ChangePiece(GameObject piece)
+
+    /*public void ChangePiece(GameObject piece)
     {
 
         if (currentArt != null) DestroyImmediate(currentArt);
 
-        currentArt = Instantiate(piece, transform.position, transform.rotation, transform.parent);
+        currentArt = Instantiate(piece, artContainer);
+        currentArt.transform.localPosition = Vector3.zero;
+    }*/
+
+    public void ChangePiece(GameObject piece)
+    {
+
+        // Create a new instance of the prefab
+        GameObject newArt = Instantiate(piece, artContainer);
+
+        // Destroy the previous instantiated object if it exists
+        if (currentArt != null)
+        {
+            Destroy(currentArt);
+        }
+
+        // Set the new instance as the currentArt
+        currentArt = newArt;
         currentArt.transform.localPosition = Vector3.zero;
     }
+
 }
